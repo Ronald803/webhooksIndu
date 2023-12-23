358ced883bb49d69b0927d7c3f0d1ab1b1c6d942
Ronald803 <ronaldblancobalboa@gmail.com>
2023-12-22 23:58:49 -0400

    multi messages added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const qrcode = require('qrcode-terminal');
-const {Client,LocalAuth} = require('whatsapp-web.js');
+const {Client,LocalAuth,MessageMedia} = require('whatsapp-web.js');
 
 const client = new Client({
   authStrategy: new LocalAuth()
@@ -25,9 +25,36 @@ client.on('message',message=>{
 
   console.log({typeMessage},{bodyMessage},{notifyNameMessage},{remoteMessageNumber},{orderTitle},{itemCount},{totalAmount1000},{totalCurrencyCode});
   console.log(message);
-  if(message.body === "hello"){
-    client.sendMessage(message.from, 'World!!!!!');
+  if(isThisWordInTheString("hola",message.body)){
+    client.sendMessage(message.from, 'Hola!😄 Somos Academia Prometeo. Te comparto las siguientes opciones de interes sobre la Academia');
+    client.sendMessage(message.from, '1. Libro *Medicina* 👈');
+    client.sendMessage(message.from, '2. Libro *Matematica* 👈');
+    client.sendMessage(message.from, '3. Tarjetas *Terminología* Médica 👈');
+    client.sendMessage(message.from, 'Escribe la opción de tu interes, ejemplo: "Medicina"');    
   }
-});
+  if(isThisWordInTheString("medicina",message.body)){
+    client.sendMessage(message.from, '*Libro 1000 MED*')
+    client.sendMessage(message.from, 'Este material es un banco de preguntas y problemas típicos de: - biología - anatomía - lenguaje - matemáticas - física - química Con todos los distintos temas incluidos en las guías otorgadas para el ingreso a la facultad de Medicina y Tecnología Médica. *Costo*: 30 BS');
+  }
+  if(isThisWordInTheString("matematica",message.body)){
+    client.sendMessage(message.from, '*Libro Matemática Preuniversitaria*')
+    client.sendMessage(message.from, 'Prepárate para ingresar a: Facultad de Ciencias Económicas y Financieras  🔹Facultad de Ingeniería 🔹Facultad de Tecnología 🔹Facultad de Medicina     Consta de todos los contenidos que se consideran en las pruebas de suficiencia académica. ❗️❗️ Teoría y práctica  ❗️❗️ Ejercicios resueltos de exámenes pasados ❗️❗️ Más de 900 ejercicios propuestos. *Costo*: 25 Bs ');
+  }
+  if(isThisWordInTheString("terminologia",message.body)){
+    client.sendMessage(message.from, '*Tarjetas de Terminología Médica*')
+    client.sendMessage(message.from, 'Son más de 100 tarjetas con los prefijos, sufijos y raíces de terminología médica que preguntan en el examen de ingreso a la facultad de medicina, enfermería, tecnología médica nutrición y dietética. Cada ficha tiene el prefijo sufijo raíz su significado, una imagen para asociar el significado, y un respectivo ejemplo de uso. *Costo*: 20 Bs.');
+  }
+  if(isThisWordInTheString("adios",message.body )){
+    client.sendMessage(message.from, 'Que tengas un resto de jornada agradable.');
+  }
 
+});
 client.initialize();
+function isThisWordInTheString (word,text){
+  const wordLower = word.toLowerCase();
+  const textLower = text.toLowerCase();
+  const arrayOfWords = textLower.split(' ');
+  let wordExists = false;
+  wordExists = arrayOfWords.some((w)=>{ return w===wordLower })
+  return wordExists
+}
