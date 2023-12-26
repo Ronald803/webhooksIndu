ec31928b4012cb564b6cd43c9c2acf7144b66ad7
Ronald803 <ronaldblancobalboa@gmail.com>
2023-12-25 23:55:42 -0400

    QR added and catalogue link added

--- index.js
+++ index.js
@@ -25,13 +25,23 @@ client.on('message',message=>{
 
   //console.log({typeMessage},{bodyMessage},{notifyNameMessage},{remoteMessageNumber},{orderTitle},{itemCount},{totalAmount1000},{totalCurrencyCode});
   //console.log(message);
+  if(typeMessage === "order"){
+    const imageUrl= 'https://tecnotek.uy/wp-content/uploads/2022/03/QR_MercadoPago-e1647385811898-768x984.png'
+    MessageMedia.fromUrl(imageUrl).then((media) => {
+      const messageText = new MessageMedia(media.mimetype, media.data, media.filename);
+      client.sendMessage(message.from, messageText);
+      setTimeout(()=>{
+        client.sendMessage(message.from, 'Deposita el 50% del costo total para reservar tu pedido');  
+      },1500)
+    })
+  }
   if(isThisWordInTheString("hola",message.body)){
     const imageUrl= 'https://scontent.flpb1-1.fna.fbcdn.net/v/t39.30808-6/314066700_184596134092273_650221308378870268_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=9c7eae&_nc_ohc=-ooihRuIztMAX_Z2ilw&_nc_ht=scontent.flpb1-1.fna&oh=00_AfDVOKaAeQ2bT-aioyVrXc_xGqRrCbNwWCmdp_yxOGU73A&oe=658B88CE'
     MessageMedia.fromUrl(imageUrl).then((media) => {
       const messageText = new MessageMedia(media.mimetype, media.data, media.filename);
       client.sendMessage(message.from, messageText);
       setTimeout(()=>{
-        client.sendMessage(message.from, 'Hola!😄 Somos Academia Prometeo. Te comparto las siguientes opciones de interes sobre la Academia\n1. Libro *Medicina* 👈\n2. Libro *Matematica* 👈\n3. Tarjetas *Terminología* Médica 👈\nEscribe la opción de tu interes, ejemplo: "Medicina"');      
+        client.sendMessage(message.from, 'Hola!😄 Somos Academia Prometeo. Te comparto las siguientes opciones de interes sobre la Academia\n1. Libro *Medicina* 👈\n2. Libro *Matematica* 👈\n3. Tarjetas *Terminología* Médica 👈\nEscribe la opción de tu interes, ejemplo: "Medicina."\nSi gustas puedes realizar tu pedido directamente de nuestro catálogo:\nhttps://wa.me/c/59175252611');      
       },1500)
     })
   }
@@ -41,7 +51,7 @@ client.on('message',message=>{
       const messageText = new MessageMedia(media.mimetype, media.data, media.filename);
       client.sendMessage(message.from, messageText);
       setTimeout(()=>{
-        client.sendMessage(message.from, '*Libro 1000 MED*\nEste material es un banco de preguntas y problemas típicos de:\n🔹 Biología\n🔹 Anatomía\n🔹 Lenguaje\n🔹 Matemáticas\n🔹 Física\n🔹 Química\nCon todos los distintos temas incluidos en las guías otorgadas para el ingreso a la facultad de Medicina y Tecnología Médica.\n*Costo*: 30 BS');  
+        client.sendMessage(message.from, '*Libro 1000 MED*\nEste material es un banco de preguntas y problemas típicos de:\n🔹 Biología\n🔹 Anatomía\n🔹 Lenguaje\n🔹 Matemáticas\n🔹 Física\n🔹 Química\nCon todos los distintos temas incluidos en las guías otorgadas para el ingreso a la facultad de Medicina y Tecnología Médica.\n*Costo*: 30 BS\nSi quieres reservar tu pedido escribe la palabra *Pagar*');  
       },1500)
     })
   }
@@ -51,7 +61,7 @@ client.on('message',message=>{
       const messageText = new MessageMedia(media.mimetype, media.data, media.filename);
       client.sendMessage(message.from, messageText);
       setTimeout(()=>{
-        client.sendMessage(message.from, '*Libro Matemática Preuniversitaria*\nPrepárate para ingresar a:\n🔹Facultad de Ciencias Económicas y Financieras\n🔹Facultad de Ingeniería\n🔹Facultad de Tecnología\n🔹Facultad de Medicina\nConsta de todos los contenidos que se consideran en las pruebas de suficiencia académica.\n❗️ Teoría y práctica \n❗️ Ejercicios resueltos de exámenes pasados\n❗️ Más de 900 ejercicios propuestos.\n*Costo*: 25 Bs ');  
+        client.sendMessage(message.from, '*Libro Matemática Preuniversitaria*\nPrepárate para ingresar a:\n🔹Facultad de Ciencias Económicas y Financieras\n🔹Facultad de Ingeniería\n🔹Facultad de Tecnología\n🔹Facultad de Medicina\nConsta de todos los contenidos que se consideran en las pruebas de suficiencia académica.\n❗️ Teoría y práctica \n❗️ Ejercicios resueltos de exámenes pasados\n❗️ Más de 900 ejercicios propuestos.\n*Costo*: 25 Bs\nSi quieres reservar tu pedido escribe la palabra *Pagar*');  
       },1500)
     })
   }
@@ -61,7 +71,17 @@ client.on('message',message=>{
       const messageText = new MessageMedia(media.mimetype, media.data, media.filename);
       client.sendMessage(message.from, messageText);
       setTimeout(()=>{
-        client.sendMessage(message.from, '*Tarjetas de Terminología Médica*\nSon más de 100 tarjetas con los prefijos, sufijos y raíces de terminología médica que preguntan en el examen de ingreso a la facultad de medicina, enfermería, tecnología médica nutrición y dietética.\nCada ficha tiene el prefijo sufijo raíz su significado, una imagen para asociar el significado, y un respectivo ejemplo de uso.\n*Costo*: 20 Bs.');  
+        client.sendMessage(message.from, '*Tarjetas de Terminología Médica*\nSon más de 100 tarjetas con los prefijos, sufijos y raíces de terminología médica que preguntan en el examen de ingreso a la facultad de medicina, enfermería, tecnología médica nutrición y dietética.\nCada ficha tiene el prefijo sufijo raíz su significado, una imagen para asociar el significado, y un respectivo ejemplo de uso.\n*Costo*: 20 Bs.\nSi quieres reservar tu pedido escribe la palabra *Pagar*');  
+      },1500)
+    })
+  }
+  if(isThisWordInTheString("Pagar",message.body)){
+    const imageUrl= 'https://tecnotek.uy/wp-content/uploads/2022/03/QR_MercadoPago-e1647385811898-768x984.png'
+    MessageMedia.fromUrl(imageUrl).then((media) => {
+      const messageText = new MessageMedia(media.mimetype, media.data, media.filename);
+      client.sendMessage(message.from, messageText);
+      setTimeout(()=>{
+        client.sendMessage(message.from, 'Deposita el 50% del costo total para reservar tu pedido');  
       },1500)
     })
   }
